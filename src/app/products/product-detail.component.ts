e0b1c64899b8a496725e86edd4d09753fbf9806b
bmansk8 <bmansk8@yahoo.com>
2020-04-10 12:40:01 -0400

    product details page
    finished the product detail component.

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 
-import {IProduct} from './product';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,27 +10,30 @@ import {IProduct} from './product';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail'
-  product: IProduct;
+  errorMessage = '';
+  product: IProduct | undefined;
 
-  constructor(private route: ActivatedRoute,
-    private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService) { }
 
   ngOnInit() {
-    let id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle+= `: ${id}`;
-    this.product = {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2019",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
+    const param = this.route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
     }
   }
 
-  onBack(): void{
+  getProduct(id: number) {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    })
+  }
+
+  onBack(): void {
     this.router.navigate(['/products']);
   }
 
